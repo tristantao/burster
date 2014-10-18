9e33ff1e9e0e196af0fbf83678e3c9ce7f579504
tristantao <tao.tristan@gmail.com>
2014-10-18 01:39:12 -0700

    updating main file

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests
 from datetime import date
 
-from page_crawler import *
-from scraper_util import *
+#from page_crawler import *
+#from scraper_util import *
+
+from page import *
 
 if __name__ == '__main__':
     universities = 'university.csv'
@@ -14,10 +16,12 @@ if __name__ == '__main__':
 
 
     for university_entry in universities:
+        university_page = Page("university_entry")
         for department in departments:
             for search_key in search_keys:
                 keywords = " ".join(university_entry, department, search_key)
                 search_result = google_search(keywords, 4)
+                for result_link in search_result:
+                    page.crawl_root(result_link, 1)
+        page.output()
 
-
-    getRawHtml
