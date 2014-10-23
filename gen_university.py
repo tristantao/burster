4689d17453b31588d9119538a93d9c0c1f222b6b
tristantao <tao.tristan@gmail.com>
2014-10-22 18:46:30 -0700

    new university scraper target

--- gen_university.py
+++ gen_university.py
@@ -5,22 +5,21 @@ import csv
 from scraper_util import *
 
 if __name__ == '__main__':
-    raw_html = getRawHtml("http://www.utexas.edu/world/univ/alpha/")
-    bs_struct = BeautifulSoup(raw_html, "html.parser")
     csv_out = csv.writer(open("universities.csv", "a"))
     csv_out.writerow(["UNIVERSITY", "URL", "STATE"])
+    start = 1
+    while start < 2100:
+        raw_html = getRawHtml("http://univ.cc/search.php?dom=edu&key=&start=%s" % start)
+        bs_struct = BeautifulSoup(raw_html, "html.parser")
+        for institution in bs_struct.find_all('li'):
+            links = institution.find_all('a', href=True)
+            if len(links) != 0:
+                link = links[0]
+                university_name = link.text.encode('utf-8')
+                url = link['href'].encode('utf-8')
+                csv_out.writerow([university_name, url])
+        start += 50
 
-    for institution in bs_struct.find_all('li'):
-        state = institution.text.encode('utf-8')
-        links = institution.find_all('a', href=True)
-        link = links[0]
-        url = link['href'].encode('utf-8')
-        university = link.text.encode('utf-8')
-        state = state.replace(university, "")
-        #print url
-        #print university
-        #print state
-        csv_out.writerow([university, url, state])
 
 
 
