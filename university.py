cd484a84513b351e2f65342b1439c29472ba1e05
tristantao <tao.tristan@gmail.com>
2014-10-23 17:34:20 -0700

    added university class

---
+++ university.py
@@ -0,0 +1,7 @@
+class University(object):
+
+    def __init__(self, university_id, name, region, url):
+        self.university_id = university_id
+        self.name = name
+        self.region = region
+        self.url = url
