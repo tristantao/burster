1f604a7b559e0351634cc7c5e97b6ba9c2b95798
tristantao <tao.tristan@gmail.com>
2014-10-23 17:34:06 -0700

    added professor class

---
+++ professor.py
@@ -0,0 +1,9 @@
+
+class Professor(object):
+
+    def __init__(self, name, email, university_id, department):
+        self.name = name
+        self.email = email
+        self.university_id = university_id
+        self.department = department
+
