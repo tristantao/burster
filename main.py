219f1b131e4fdfe5f1fc695b89997d1d7c622791
tristantao <tao.tristan@gmail.com>
2014-10-23 17:34:40 -0700

    main driver uses professor and university and db_util

--- main.py
+++ main.py
@@ -16,19 +16,25 @@ import keys
 if __name__ == '__main__':
     universities = db_util.get_university_from_db()
 
-    departments = ['statistics', 'mathematics', 'computer science']
-    search_keys = ['department faculty', 'career']
+    departments = ['statistics', 'mathematics', 'computer science', 'physics', '']
+    search_keys = ['department faculty']
 
-    for university_name, university_url in universities.iteritems():
-        page = page.Page(university_url, university_name)
+    for university in universities:
+
+        university_id, university_name = university.university_id, university.name,
+        university_region, university_url = university.region, university.url
+
+        page = page.Page(university_url, university_id)
         for department in departments:
             for search_key in search_keys:
                 keywords = " ".join([university_name, department, search_key])
                 search_results, search_next_link = search_util.bing_search(keywords, keys.bing_id, first_n=5)
                 for result_object in search_results:
-                    page.crawl_node(result_object.url, 1)
+                    page.crawl_node(result_object.url, 0) #currently scrape depth 0, only surface
+                    professor_list = page.output_professors()
+                    db_util.insert_professors(professor_list)
         pdb.set_trace()
-        page.output()
+
         #break
 
 
