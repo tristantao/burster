3cca676de8c652ef32294e74f7f490b35a3d5aa8
tristantao <tao.tristan@gmail.com>
2014-11-05 16:57:00 -0800

    rough draft roudn roboin

--- search_util.py
+++ search_util.py
@@ -62,9 +62,68 @@ def name_from_email(email, school_name, bing_id, first_n=3):
 
 
 #name_from_email("anirbanb@stat.tamu.edu", "Texas A&M University", bing_id=keys.bing_id)
-print name_from_email("rmclaughlin@walsh.edu", "Walsh University", bing_id=keys.bing_id)
-
+#print name_from_email("rmclaughlin@walsh.edu", "Walsh University", bing_id=keys.bing_id)
 #name_from_email("dcline@stat.tamu.edu", "")
+
+
+class ElementToken:
+    def __init__(self, starting_node, direction):
+        #direction has to be next or previous
+        self.root = starting_node
+        if direction == "previous" or direction == "next":
+            self.direction = direction
+        else:
+            raise Exception("Bad Direction")
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        next = self.root.previous_element if self.direction == "previous" else self.root.next_element
+        if next == None:
+            raise StopIteration
+        else:
+            return next
+
+
+def roundrobin(*iterables):
+    pending = len(iterables)
+    nexts = cycle(iter(it).next for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = cycle(islice(nexts, pending))
+
+
+def test_name_extraction(page, email):
+    raw_html = getRawHtml(page)
+    name = email.split("@")[0] if "@" in email else email
+    bs_struct = BeautifulSoup(raw_html, "html.parser")
+    name_regex = r'' + name
+    for elem in bs_struct(text=re.compile(name_regex)):
+        print "eleme " + elem
+        print elem.parent
+        element_prev_token = ElementToken(elem, "previous")
+        element_next_token = ElementToken(elem, "next")
+
+        for element in roundrobin((element_prev_token, element_next_token))
+    return None
+
+
+
+print test_name_extraction('http://www.cs.csub.edu/faculty.php', 'vvakilian@csub.edu')
+
+
+
+
+
+
+
+
+
 
 
 
